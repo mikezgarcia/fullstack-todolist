fe9e2641eebe1c25c252234c56df8838193dcc38
Michael Garcia <michaelgarcia101@gmail.com>
2022-09-24 13:22:26 +0800

    Initial commit

---
+++ src/test/java/com/example/fullstacktodolist/FullstackTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstacktodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
